ff673e6e47c3efcc9a8965799086c945ab141735
michael-tanner-coder <michaelmontytanner@gmail.com>
2021-08-29 22:04:09 -0500

    Octo boss now moves in a figure 8 pattern

--- js/enemy/boss_octopus.js
+++ js/enemy/boss_octopus.js
@@ -22,12 +22,30 @@ function bossOctopusClass() {
   this.health = 100;
   this.collW = this.collH = 64;
   this.readyToRemove = false;
+  this.pos_x = 128;
+  this.pos_y = 120;
+  this.theta = 0;
+  this.d_theta = Math.PI / (3 * 30);
+  this.xrad = 30;
+  this.yrad = 30;
+  this.x_start = this.pos_x;
+  this.y_start = this.pos_y;
 
   this.reset = function () {};
 
   this.move = function () {
-    this.tentacleShotTimer -= 1;
+    this.pos_x = this.x_start + this.xrad * Math.sin(this.theta + Math.PI / 2);
+    this.pos_y = this.y_start + this.yrad * Math.sin(2 * this.theta);
+    this.theta += this.d_theta;
+    if (this.theta >= 2 * Math.PI) {
+      this.theta -= 2 * Math.PI;
+    }
+    2;
     this.mainShotTimer -= 1;
+    this.tentacleShotTimer -= 1;
+
+    this.updateShootingPositions();
+
     if (this.tentacleShotTimer <= 0) {
       this.tentacleShotTimer = this.defaultTentacleShotTimer;
       this.shoot();
@@ -37,6 +55,29 @@ function bossOctopusClass() {
       this.mainShotTimer = this.defaultMainShotTImer;
       this.shootFromFace();
     }
+  };
+
+  this.updateShootingPositions = function () {
+    this.tentacle_positions[0].x = this.pos_x - 128;
+    this.tentacle_positions[0].y = this.pos_y - 104;
+
+    this.tentacle_positions[1].x = this.pos_x - 120;
+    this.tentacle_positions[1].y = this.pos_y - 56;
+
+    this.tentacle_positions[2].x = this.pos_x - (128 - 96);
+    this.tentacle_positions[2].y = this.pos_y + 28;
+
+    this.tentacle_positions[3].x = this.pos_x + 20;
+    this.tentacle_positions[3].y = this.pos_y + 28;
+
+    this.tentacle_positions[4].x = this.pos_x + 120;
+    this.tentacle_positions[4].y = this.pos_y - 104;
+
+    this.tentacle_positions[5].x = this.pos_x + 120;
+    this.tentacle_positions[5].y = this.pos_y - 56;
+
+    this.x = this.pos_x;
+    this.y = this.pos_y - 60;
   };
 
   this.shoot = function () {
@@ -59,10 +100,14 @@ function bossOctopusClass() {
   };
 
   this.draw = function () {
-    drawAnimFrame(OCTOPUS_IMAGE_NAME, 128, 120, this.frame, 256, 240);
-    // this.tentacle_positions.forEach((tentacle) => {
-    //   context.fillRect(tentacle.x, tentacle.y, 16, 16);
-    // });
+    drawAnimFrame(
+      OCTOPUS_IMAGE_NAME,
+      this.pos_x,
+      this.pos_y,
+      this.frame,
+      256,
+      240
+    );
   };
 
   this.animate = function () {
