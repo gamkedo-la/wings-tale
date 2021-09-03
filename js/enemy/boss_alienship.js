059c2ed6851b4d8afff405d3ba36c61f67a7069c
Armando Navarrete <viking805fan@yahoo.com>
2021-09-03 13:37:40 -0700

    Alienship Boss - Updated X/Y axis movement and also implemented projectiles to shoot from each turret. (W.I.P.)

--- js/enemy/boss_alienship.js
+++ js/enemy/boss_alienship.js
@@ -5,22 +5,32 @@ bossAlienshipClass.prototype = new moveDrawClass();
 
 function bossAlienshipClass() {
   this.xv = 2;
+  this.yv = .5;
 
   this.reset = function () {
-    this.x = 0;
+    this.x = 50;
     this.y = 120;
   };
 
   this.move = function () {
     this.x += this.xv;
-    if (this.x > GAME_W) {
+    if (this.x > GAME_W / 1.2) {
       this.xv = -this.xv;
     }
-    if (this.x < 0) {
+    if (this.x < 50 / 1.2) {
       this.xv = -this.xv;
+    }
+  
+    this.y += this.yv;
+    if (this.y > 120) {
+      this.yv = -this.yv;
+    }
+    if (this.y < 70) {
+      this.yv = -this.yv;
     }
   };
 
+  
   this.draw = function () {
     drawAnimFrame(ALIENSHIP_IMAGE_NAME, this.x, this.y, this.frame, 256, 240);
 
@@ -32,8 +42,12 @@ function bossAlienshipClass() {
         context.globalCompositeOperation = "source-over"; // restore to default
     }
 
-    if (50 * Math.random() < 1) {
-      new enemyShotClass(this.x, this.y);
+    if (50 * Math.random() < 2) {
+      new enemyShotClass(this.x , this.y + 10);
+      new enemyShotClass(this.x - 60, this.y - 10);
+      new enemyShotClass(this.x - 90, this.y - 55);
+      new enemyShotClass(this.x + 60, this.y - 10);
+      new enemyShotClass(this.x + 90, this.y - 55);
     }
     for (var i = 0; i < ALIENSHIP_FRAMES; i++) {
       this.neckAngles[i] += 0.01 * Math.cos(this.neckAnglesOsc[i]);
