63b20ed59de72b4e0b2c11452727b59598fb4063
michael-tanner-coder <michaelmontytanner@gmail.com>
2021-10-09 15:25:48 -0500

    Laser shots no longer stay on screen after power up duration

--- js/player/shot.js
+++ js/player/shot.js
@@ -44,7 +44,7 @@ function shotClass(
     this.x += this.xv;
     this.y += this.yv;
     if (this.y < 0 || this.x < 0 || this.x > GAME_W || this.y > GAME_H || 
-          (playerWhoShot.hasLaserPowerUp && this.lifetimeIfLaser--<0)) {
+          (this.shotLength === LASER_SHOT_LENGTH && this.lifetimeIfLaser--<0)) {
       this.readyToRemove = true;
       // a bullet flying off screen resets the player's combo counter
       if (this.ownedByPlayer) this.ownedByPlayer.combo.reset();
